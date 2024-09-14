cb5e394ff14a3f4bccf66a3af35101b362bd9c4d
dstrzykalski <dstrzykalski@yahoo.com>
2024-09-13 23:38:27 -0400

    Starter React

--- counter.js
+++ counter.js
@@ -1,9 +1,5 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+function greet() {
+  console.log("Daved's Portfolio");
 }
+
+greet();
