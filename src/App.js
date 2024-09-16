051e6452c53eeee4ee5ba391ceaf2cf535bb29bc
dstrzykalski <dstrzykalski@yahoo.com>
2024-09-16 16:45:09 -0400

    Added Nav Bar --- Needs Editing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import React from 'react';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
       {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar () {
+    return (
+        <nav className="navbarMain">
+            <div className="navbar">
+                <a href="#home">Home</a>
+                <a href="#about">About Me</a>
+                <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
